810403ef707743ed16e2ebf7fc711b8a61afbf2b
giox367 <111007060+Giox367@users.noreply.github.com>
2022-09-28 08:22:00 +0200

    .

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 let budget;
-budget = 100;
+budget = Number(prompt("Enter yor budget: "));
 
-function decrease() {
+function decreaseHalf() {
   for (let i = budget; i >= 0; i--) {
-    if (i === 50) {
-      document.getElementById("half-budget").innerHTML = "Sei a metà del budget";
+    if (i === parseInt(budget / 2)) {
+      document.getElementById("spent").innerHTML = `Sei a metà del budget, hai ora ${parseInt(budget / 2)} €`;
     }
+  }
+}
+function decreaseMore() {
+  for (let i = budget; i >= 0; i--) {
+    if (i === parseInt(budget / 10)) {
+      document.getElementById("spent2").innerHTML = `Stai per finire i soldi, hai ancora ${parseInt(budget / 10)} €`;
+    }
+  }
+}
+function decreaseLast() {
+  for (let i = budget; i >= 0; i--) {
     if (i === 0) {
-      document.getElementById("spent").innerHTML = "Hai finito il budget";
+      document.getElementById("spent3").innerHTML = "Stai n'guaiato frà";
     }
   }
 }
